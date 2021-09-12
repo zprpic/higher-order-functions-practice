4583b74b4dbdb663657a4c71cbad761e636a6cdc
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-12 11:17:40 +0200

    Playing around with higher order functions and objects/array

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const JSONdata = require("./JSON-data/mockturtle.json");
 
 // data = [ {...}, {...}, {...}, ...];
 
-const allNamesFromArray = JSONdata.map((data) => data["full name"]);
+// const allNamesFromArray = JSONdata.map((data) => data["full name"]); // [ "full name", "full name", "full name", ...]
 
-console.log(allNamesFromArray);
+/* const namesAndCities = JSONdata.map((data) => {
+  return `${data["full name"]}, city: ${data.address.city}}`; // [full name, city, ...]
+}); */
+
+const namesAndCities = JSONdata.map((data) => {
+  const name = data["full name"];
+  const { address } = data;
+  return { name, address };
+});
+
+console.log(namesAndCities);
